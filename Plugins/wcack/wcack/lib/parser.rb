ee3a809981bf96fce446c79e8bde808f1f35a2f6
Roben Kleene <robenk@gmail.com>
2013-06-04 21:49:19 -0400

    Refactoring test results parsing

--- Plugins/wcack/wcack/lib/parser.rb
+++ Plugins/wcack/wcack/lib/parser.rb
@@ -1,23 +1,31 @@
-class ParsedLine
-  attr_reader :file_path, :line_number
+module WcAck
+  def self.load data
+    puts data
+    
+  end
 
-  ANSI_ESCAPE = '\x1b[^m]*m'
-  ANSI_WRAPPER_REGEXP = Regexp.new("#{ANSI_ESCAPE}" + '(.+?)' + "#{ANSI_ESCAPE}")
-  METADATA_REGEXP = Regexp.new(ANSI_WRAPPER_REGEXP.source + ":#{ANSI_ESCAPE}[0-9]+#{ANSI_ESCAPE}:")
 
-  def initialize(line)
+  class ParsedLine
+    attr_reader :file_path, :line_number
+
+    ANSI_ESCAPE = '\x1b[^m]*m'
+    ANSI_WRAPPER_REGEXP = Regexp.new("#{ANSI_ESCAPE}" + '(.+?)' + "#{ANSI_ESCAPE}")
+    METADATA_REGEXP = Regexp.new(ANSI_WRAPPER_REGEXP.source + ":#{ANSI_ESCAPE}[0-9]+#{ANSI_ESCAPE}:")
+
+    def initialize(line)
 puts "line = " + line.to_s
 
 
-    ansi_wrapped = line.scan(ANSI_WRAPPER_REGEXP)
-    @file_path = ansi_wrapped[0][0]
-    @line_number = ansi_wrapped[1][0].to_i
+      ansi_wrapped = line.scan(ANSI_WRAPPER_REGEXP)
+      @file_path = ansi_wrapped[0][0]
+      @line_number = ansi_wrapped[1][0].to_i
 
 puts "ansi_wrapped = " + ansi_wrapped.to_s
 
 
-    match = line.sub(METADATA_REGEXP, '')
+      match = line.sub(METADATA_REGEXP, '')
 
-    puts match.inspect
+puts match.inspect
+    end
   end
 end
--- Plugins/wcack/wcack/tests/tc_parser.rb
+++ Plugins/wcack/wcack/tests/tc_parser.rb
@@ -2,43 +2,41 @@ require "test/unit"
 require "yaml"
 
 PARSER_FILE = File.join(File.dirname(__FILE__), '..', 'lib', 'parser')
-puts PARSER_FILE
 require PARSER_FILE
 
 TEST_DIRECTORY=File.join(File.dirname(__FILE__), 'data')
 TEST_DATA_GENERATED=File.join(TEST_DIRECTORY, 'test_data_generated.yml')
-TEST_DATA=File.join(TEST_DIRECTORY, 'test_data.yml')
 
 class TestWCACK < Test::Unit::TestCase
   def test_test_data_generated
     test_file_hash = YAML.load_file(TEST_DATA_GENERATED)
-    test_data = TestDataFactory.test_data_from_test_file_hash(test_file_hash)
-    test_result = TestDataFactory.test_result_from_test_file_hash(test_file_hash)
+    
+    # test_results = 
 
-    parse_line = ParsedLine.new(test_data.line)
+    test_data = TestDataFactory.get_input_data(test_file_hash)
 
-    assert_equal(parse_line.file_path, test_result.filename, "File path doesn't match.")
-    assert_equal(parse_line.line_number, test_result.line_number, "Line number doesn't match.")
+    test_results = TestDataFactory.test_result_from_test_file_hash(test_file_hash)
+
+    # WcACK.load()
+
+    # parse_line = ParsedLine.new(test_data.line)
+
+    # assert_equal(parse_line.file_path, test_result.filename, "File path doesn't match.")
+    # assert_equal(parse_line.line_number, test_result.line_number, "Line number doesn't match.")
   end 
 end
 
 # Helpers
 
+INPUT_FILE_KEY = "input_file"
+RESULTS_KEY = "results"
 class TestDataFactory
-  def self.test_data_from_test_file_hash(test_file_hash)
-    TestData.new(test_file_hash["test"])
+  def self.get_input_data(hash)
+    input_file=File.join(TEST_DIRECTORY, hash[INPUT_FILE_KEY])
+    `cat "#{input_file}"`
   end
   def self.test_result_from_test_file_hash(test_file_hash)
-    TestResult.new(test_file_hash["result"])
-  end
-end
-
-class TestData
-  attr_reader :term, :line
-  def initialize(test_data)
-    line_output_file=File.join(TEST_DIRECTORY, test_data["lineoutputfile"])
-    @line = `cat "#{line_output_file}"`
-    @term = test_data["term"]
+    TestResult.new(test_file_hash[RESULTS_KEY])
   end
 end
 
