293ba4cb00f61f059ee6654f6e226fd8dbe22bbc
Roben Kleene <robenk@gmail.com>
2013-06-05 21:07:37 -0400

    Work on parser

--- Plugins/wcack/wcack/lib/parser.rb
+++ Plugins/wcack/wcack/lib/parser.rb
@@ -1,30 +1,30 @@
+MODEL_FILE = File.join(File.dirname(__FILE__), 'model')
+require MODEL_FILE
+
 module WcAck
+
   def self.load data
 
     just_one = true
     data.each_line { |line|
       if just_one
-        parsed_line = ParsedLine.new(line)
-        puts parsed_line
+        self.file_from_line(line)
         just_one = false
       end
     }    
   end
 
-  class ParsedLine
-    attr_reader :file_path, :line_number
+  ANSI_ESCAPE = '\x1b[^m]*m'
+  ANSI_WRAPPER_REGEXP = Regexp.new("#{ANSI_ESCAPE}" + '(.+?)' + "#{ANSI_ESCAPE}")
+  METADATA_REGEXP = Regexp.new(ANSI_WRAPPER_REGEXP.source + ":#{ANSI_ESCAPE}[0-9]+#{ANSI_ESCAPE}:")
 
-    ANSI_ESCAPE = '\x1b[^m]*m'
-    ANSI_WRAPPER_REGEXP = Regexp.new("#{ANSI_ESCAPE}" + '(.+?)' + "#{ANSI_ESCAPE}")
-    METADATA_REGEXP = Regexp.new(ANSI_WRAPPER_REGEXP.source + ":#{ANSI_ESCAPE}[0-9]+#{ANSI_ESCAPE}:")
-
-    def initialize(line)
+  def self.file_from_line(line)
 puts "line = " + line.to_s
 
 
       ansi_wrapped = line.scan(ANSI_WRAPPER_REGEXP)
-      @file_path = ansi_wrapped[0][0]
-      @line_number = ansi_wrapped[1][0].to_i
+      file_path = ansi_wrapped[0][0]
+      line_number = ansi_wrapped[1][0].to_i
 
 puts "ansi_wrapped = " + ansi_wrapped.to_s
 
@@ -32,6 +32,6 @@ puts "ansi_wrapped = " + ansi_wrapped.to_s
       match = line.sub(METADATA_REGEXP, '')
 
 puts match.inspect
-    end
+    
   end
 end
