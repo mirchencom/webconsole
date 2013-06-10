4e54e3a068d4b53d5e7fbe3e42653f8a55910127
Roben Kleene <robenk@gmail.com>
2013-06-09 21:09:35 -0400

    Adding a dom element

--- Plugins/wcack/prototypes/dynamic_view/js/wcack.js
+++ Plugins/wcack/prototypes/dynamic_view/js/wcack.js
@@ -1,4 +1,40 @@
-window.addEventListener('load', function () { setup(); }, false);
-function setup() {
-	
+Mousetrap.bind(['command+n'], function(e) {
+    e.preventDefault();
+	testCall()
+    return false;
+});
+
+function testCall() {
+	// Add a dom element here
+	$("<a/>", {
+	    id: 'example-link',
+	    href: 'http://www.example.com/',
+	    text: 'Example Page'
+	}).appendTo("body");
+	console.log("test2");
 }
+
+// function highlight(line) {
+//      var lines = line instanceof Array ? line : [line],
+//          i,
+//          code;
+// 
+//      for (i = 0; i < lines.length; ++i) {
+//          code = document.getElementById('line-' + lines[i]);
+//          code.className = 'highlight';
+// 
+//          if (timeouts[lines[i]]) {
+//              clearTimeout(timeouts[lines[i]]);
+//              delete timeouts[lines[i]];
+//          }
+// 
+//          timeouts[lines[i]] = setTimeout(function() {
+//              _remove(lines);
+//          }, 1500);
+//      }
+//  }
+
+// window.addEventListener('load', function () { setup(); }, false);
+// function setup() {
+// 	
+// }
