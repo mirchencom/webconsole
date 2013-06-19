b35fd6c23585968cc99a5983e084aa390e05c941
Roben Kleene <robenk@gmail.com>
2013-06-18 21:46:23 -0400

    Passing parameter to applescript example working

--- Web Console/Web ConsoleTests/Web_ConsoleScriptTests.m
+++ Web Console/Web ConsoleTests/Web_ConsoleScriptTests.m
@@ -14,6 +14,7 @@
 
 @interface Web_ConsoleScriptTests ()
 + (BOOL)windowWithWindowNumberExists:(NSInteger)windowNumber;
++ (NSAppleScript *)appleScriptFromTestBundleWithName:(NSString *)name;
 @end
 
 @implementation Web_ConsoleScriptTests
@@ -32,26 +33,75 @@
 
 - (void)testLoadHTML
 {
-    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    NSAppleScript *appleScript = [[self class] appleScriptFromTestBundleWithName:kTestScriptLoadHTMLFilename];
 
-    NSLog(@"[bundle resourcePath] = %@", [bundle resourcePath]);
-
-    NSURL *testLoadHTMLFileURL = [bundle URLForResource:kTestScriptLoadHTMLFilename
-                                          withExtension:kTestScriptsExtension
-                                           subdirectory:kTestScriptsSubdirectory];
-    
     NSDictionary *errorInfo;
-    NSAppleScript *script = [[NSAppleScript alloc] initWithContentsOfURL:testLoadHTMLFileURL error:&errorInfo];
+    NSAppleEventDescriptor *result = [appleScript executeAndReturnError:&errorInfo];
 
     STAssertNil(errorInfo, @"errorInfo should be nil.");
     
-    NSAppleEventDescriptor *result = [script executeAndReturnError:&errorInfo];
     NSInteger ID = [[[result descriptorForKeyword:'seld'] stringValue] intValue];
     BOOL windowNumberExists = [[self class] windowWithWindowNumberExists:ID];
-
-    STAssertNil(errorInfo, @"errorInfo should be nil.");
     
-    STAssertTrue(windowNumberExists, @"Window should exist.");    
+    STAssertTrue(windowNumberExists, @"Window should exist.");
+}
+
+
+
+#ifndef kASAppleScriptSuite
+#define kASAppleScriptSuite 'ascr'
+#endif
+
+#ifndef kASSubroutineEvent
+#define kASSubroutineEvent 'psbr'
+#endif
+
+#ifndef keyASSubroutineName
+#define keyASSubroutineName 'snam'
+#endif
+
+- (void)testAppleScript {
+
+    NSAppleScript *appleScript = [[self class] appleScriptFromTestBundleWithName:@"Test Arguments"];
+    
+    NSAppleEventDescriptor *firstParameter = [NSAppleEventDescriptor descriptorWithString:@"Message from my app."];
+
+    NSAppleEventDescriptor *parameters = [NSAppleEventDescriptor listDescriptor];
+    [parameters insertDescriptor:firstParameter atIndex:1];
+
+    // create the AppleEvent target
+    ProcessSerialNumber psn = {0, kCurrentProcess};
+    NSAppleEventDescriptor *target = [NSAppleEventDescriptor descriptorWithDescriptorType:typeProcessSerialNumber
+                                                                                    bytes:&psn
+                                                                                   length:sizeof(ProcessSerialNumber)];
+    // create an NSAppleEventDescriptor with the script's method name to call,
+    // this is used for the script statement: "on show_message(user_message)"
+    // Note that the routine name must be in lower case.
+    NSAppleEventDescriptor* handler = [NSAppleEventDescriptor descriptorWithString:[@"show_message" lowercaseString]];
+
+
+    // create the event for an AppleScript subroutine,
+    // set the method name and the list of parameters
+    NSAppleEventDescriptor *event = [NSAppleEventDescriptor appleEventWithEventClass:kASAppleScriptSuite
+                                             eventID:kASSubroutineEvent
+                                    targetDescriptor:target
+                                            returnID:kAutoGenerateReturnID
+                                       transactionID:kAnyTransactionID];
+    [event setParamDescriptor:handler forKeyword:keyASSubroutineName];
+    [event setParamDescriptor:parameters forKeyword:keyDirectObject];
+
+
+//        NSAppleEventDescriptor *result = [script executeAndReturnError:&errorInfo];
+    // call the event in AppleScript
+//    if (![appleScript executeAppleEvent:event error:&errors]);
+//    {
+//        // report any errors from 'errors'
+//    }
+
+    NSDictionary *errorInfo;
+    NSAppleEventDescriptor *result = [appleScript executeAppleEvent:event error:&errorInfo];
+
+    NSLog(@"result = %@", result);
 }
 
 #pragma mark - Helpers
@@ -69,6 +119,18 @@
     return windowExists;
 }
 
++ (NSAppleScript *)appleScriptFromTestBundleWithName:(NSString *)name {
+    NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+    NSURL *testLoadHTMLFileURL = [bundle URLForResource:name
+                                          withExtension:kTestScriptsExtension
+                                           subdirectory:kTestScriptsSubdirectory];
+    
+    NSDictionary *errorInfo;
+    NSAppleScript *appleScript = [[NSAppleScript alloc] initWithContentsOfURL:testLoadHTMLFileURL error:&errorInfo];
+    
+    NSAssert(!errorInfo, @"errorInfo should be nil.");
 
+    return appleScript;
+}
 
 @end
