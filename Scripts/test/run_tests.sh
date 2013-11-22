8cab29c034094a064f81c5aa04653ec728f2a397
Roben Kleene <robenk@gmail.com>
2013-11-21 23:27:57 -0500

    Run long tests with flag

--- Scripts/test/run_tests.sh
+++ Scripts/test/run_tests.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
-rake
+usage () {
+	echo "Usage: run_tests.sh [-l]"
+	echo
+	echo "-l : Run long tests"
+}
+
+LONG=false
+while getopts lh option
+do
+	case "$option"
+	in
+	    l)  LONG=true
+			;;
+	    h)  usage
+	        exit 0
+	        ;;
+	    \?) echo "Invalid option or missing argument"
+			usage
+	        exit 1
+	        ;;
+	esac
+done
+
+if $LONG ; then
+	rake long
+else
+	rake
+fi
