493332e0368212da01cac1a216748630aebebcf9
Roben Kleene <robenk@gmail.com>
2013-11-24 23:55:52 -0500

    Quick hack to get wcack tc_parser.rb working in TextMate

--- Web Console/Ack/wcack/test/lib/test_data_helper.rb
+++ Web Console/Ack/wcack/test/lib/test_data_helper.rb
@@ -6,19 +6,35 @@ TEST_SCRIPTS_DIRECTORY = File.join(TEST_DATA_HELPER_DIRECTORY, "..", "scripts")
 module TestHelper
 
   module TestData
-    TEST_ACK_OUTPUT_FILE = File.join(TEST_SCRIPTS_DIRECTORY, "test_ack_output.rb")
+
+    OUTPUT_DIRECTORY = File.join(TEST_SCRIPTS_DIRECTORY, "output")
+    TEST_ACK_OUTPUT_OUTPUT_FILE = File.join(OUTPUT_DIRECTORY, "test_ack_output")
     def self.test_ack_output
-      command = Shellwords.escape(TEST_ACK_OUTPUT_FILE)
+      command = "cat #{Shellwords.escape(TEST_ACK_OUTPUT_OUTPUT_FILE)}"
+      result = `#{command}`
+      return result
+    end
+    TEST_DATA_JSON_OUTPUT_FILE = File.join(OUTPUT_DIRECTORY, "test_data_json.json")
+    def self.test_data_json
+      command = "cat #{Shellwords.escape(TEST_DATA_JSON_OUTPUT_FILE)}"
       result = `#{command}`
       return result
     end
 
-    TEST_DATA_JSON_FILE = File.join(TEST_SCRIPTS_DIRECTORY, "test_data_json.rb")
-    def self.test_data_json
-      command = Shellwords.escape(TEST_DATA_JSON_FILE)
-      result = `#{command}`
-      return result
-    end
+
+    # TODO Above is a quick hack replace of below because below wouldn't run in TextMate's run command window
+    # TEST_ACK_OUTPUT_FILE = File.join(TEST_SCRIPTS_DIRECTORY, "test_ack_output.rb")    
+    # def self.test_ack_output
+    #   command = Shellwords.escape(TEST_ACK_OUTPUT_FILE)
+    #   result = `#{command}`
+    #   return result
+    # end  
+    # TEST_DATA_JSON_FILE = File.join(TEST_SCRIPTS_DIRECTORY, "test_data_json.rb")
+    # def self.test_data_json
+    #   command = Shellwords.escape(TEST_DATA_JSON_FILE)
+    #   result = `#{command}`
+    #   return result
+    # end
   end
 
 end
