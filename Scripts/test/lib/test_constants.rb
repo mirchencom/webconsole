904c8487ce9105a477319e683f13caf83aaada7b
Roben Kleene <robenk@gmail.com>
2013-12-03 23:38:23 -0500

    Removed some unnecessary instances of expand_path

--- Scripts/test/lib/test_constants.rb
+++ Scripts/test/lib/test_constants.rb
@@ -1,4 +1,4 @@
-TEST_LIB_DIRECTORY = File.expand_path(File.dirname(__FILE__))
+TEST_LIB_DIRECTORY = File.dirname(__FILE__)
 TEST_HELPER_FILE = File.join(TEST_LIB_DIRECTORY, "test_helper")
 TEST_DATA_DIRECTORY = File.join(TEST_LIB_DIRECTORY, "..", "data")
 PAUSE_TIME = 0.5
