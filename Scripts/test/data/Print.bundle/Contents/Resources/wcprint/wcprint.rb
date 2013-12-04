20c6316e2f322d225846f1a90a68a1f7c89cbcb6
Roben Kleene <robenk@gmail.com>
2013-12-04 00:22:43 -0500

    Removed unnecessary constants

--- Scripts/test/data/Print.bundle/Contents/Resources/wcprint/wcprint.rb
+++ Scripts/test/data/Print.bundle/Contents/Resources/wcprint/wcprint.rb
@@ -2,15 +2,14 @@
 
 require 'webconsole'
 
-BASE_DIRECTORY = File.dirname(__FILE__)
-LIB_DIRECTORY = File.join(BASE_DIRECTORY, "lib")
+LIB_DIRECTORY = File.join(File.dirname(__FILE__), "lib")
 CONTROLLER_FILE = File.join(LIB_DIRECTORY, "controller")
 require CONTROLLER_FILE
 
 # Window Manager
 window_id = ENV['WINDOWID']
 window_manager = WebConsole::WindowManager.new(window_id)
-BASE_PATH = File.expand_path(BASE_DIRECTORY)
+BASE_PATH = File.expand_path(File.dirname(__FILE__))
 window_manager.base_url_path = BASE_PATH
 
 # Controller
