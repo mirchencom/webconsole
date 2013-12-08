a7b0c71239f317bef3b9abcbf3a43e335dbc7af0
Roben Kleene <robenk@gmail.com>
2013-12-07 22:45:36 -0500

    Test data for wcsearch now uses grep instead of ack

--- Web Console/Search/wcsearch/test/lib/test_script_constants.rb
+++ Web Console/Search/wcsearch/test/lib/test_script_constants.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), "../..", "lib", "constants")
 SEARCH_TERM = "ei.*?od"
 TEST_DATA_DIRECTORY = File.join(File.dirname(__FILE__), "..", "data")
-TEST_DATA_SEARCH_COMMAND = "ack -o --with-filename --noheading --nocolor"
+# TEST_DATA_SEARCH_COMMAND = "ack -o --with-filename --noheading --nocolor"
+TEST_DATA_SEARCH_COMMAND = "grep --recursive --line-number --only-matching --extended-regexp"
--- Web Console/Search/wcsearch/test/scripts/test_data_json.rb
+++ Web Console/Search/wcsearch/test/scripts/test_data_json.rb
@@ -17,12 +17,23 @@ matches = []
 match_lines.each_line do |line_match|
   match_hash = Hash.new
 
-  file_path = /(.*):.*:.*/.match(line_match).captures[0]
-  line_number = /.*:(.*):.*/.match(line_match).captures[0]
-  matched_text = /.*:.*:(.*)/.match(line_match).captures[0]
+  result = /(.*):(.*):(.*)/.match(line_match)
+  if result
+    file_path = result.captures[0]
+    line_number = result.captures[1]
+    matched_text = result.captures[2]
+  else
+    # Grep doesn't add the full metadata in front of subsequent matches in the same file
+    # So construct the match from the previous match
+    previous_match = matches.last
+    file_path = previous_match[FILE_PATH_KEY]
+    line_number = previous_match[LINE_NUMBER_KEY]
+    matched_text = line_match.chomp
+  end
 
+  display_file_path = Pathname.new(file_path).relative_path_from(Pathname.new(TEST_DATA_DIRECTORY)).to_s
   match_hash[FILE_PATH_KEY] = File.expand_path(file_path)
-  match_hash[DISPLAY_FILE_PATH_KEY] = Pathname.new(file_path).relative_path_from(Pathname.new(TEST_DATA_DIRECTORY)).to_s
+  match_hash[DISPLAY_FILE_PATH_KEY] = display_file_path
   match_hash[LINE_NUMBER_KEY] = line_number
   match_hash[MATCHED_TEXT_KEY] = matched_text
   matches << match_hash
