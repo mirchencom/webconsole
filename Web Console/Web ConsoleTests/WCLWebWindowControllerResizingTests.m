a6accfa2cfd9b2f664c5491631c1686a8ca9acdd
Roben Kleene <robenk@gmail.com>
2013-12-30 22:56:27 -0500

    Adding code to test rect equality

--- Web Console/Web ConsoleTests/WCLWebWindowControllerResizingTests.m
+++ Web Console/Web ConsoleTests/WCLWebWindowControllerResizingTests.m
@@ -16,8 +16,51 @@
 
 #import "WCLPluginManager.h"
 
-@interface WCLWebWindowControllerResizingTests : WCLWebWindowControllerTestCase
+//#define kTestWindowFrame NSRectMake(158, 97, 303, 449)
+//#define kTestWindowFrameTwo NSRectMake(50, 50, 343, 409)
 
+
+//bool NSRect::equals(const NSRect& rect) const
+//{
+//    return (origin.equals(rect.origin) &&
+//            size.equals(rect.size));
+//}
+//
+//bool NSPoint::equals(const NSPoint& target) const
+//{
+//    return ((x == target.x) && (y == target.y));
+//}
+//
+//bool NSSize::equals(const NSSize& target) const
+//{
+//    return ((width == target.width) && (height == target.height));
+//}
+
+NS_INLINE BOOL NSSizeEqualToSize (NSSize size1, NSSize size2)
+{
+    return (size1.height == size2.height) && (size1.width == size2.width);
+}
+
+NS_INLINE BOOL NSPointEqualToPoint (NSPoint point1, NSPoint point2)
+{
+    return (point1.x == point2.x) && (point1.y == point2.y);
+}
+
+NS_INLINE BOOL NSRectEqualToRect (NSRect rect1, NSRect rect2)
+{
+    return NSPointEqualToPoint(rect1.origin, rect2.origin) && NSSizeEqualToSize(rect1.size, rect2.size);
+}
+
+//NS_INLINE NSRange NSMakeRange(NSUInteger loc, NSUInteger len) {
+//    NSRange r;
+//    r.location = loc;
+//    r.length = len;
+//    return r;
+//}
+
+
+@interface WCLWebWindowControllerResizingTests : WCLWebWindowControllerTestCase
++ (NSRect)savedFrameNamed:(NSString *)frameName;
 @end
 
 @implementation WCLWebWindowControllerResizingTests
@@ -34,6 +77,8 @@
 
 - (void)testResizingWindow
 {
+#warning This implementation will overwrite my real window frame data for the test plugin, so I want to use a different name here, maybe use the load plugin API and the print plugin?
+    
     // For window resizing to work the plugin must have a name
     WCLPlugin *plugin = [[WCLPluginManager sharedPluginManager] pluginWithName:kTestPluginName];
     XCTAssertNotNil(plugin.name, @"The WCLPlugin should have a name.");
@@ -53,9 +98,48 @@
     
     // Test Resizing...
     
+    // Get the window frame from NSUserDefaults
+
+    NSRect savedFrame = [[self class] savedFrameNamed:plugin.name];
+    NSLog(@"savedFrame = %@", NSStringFromRect(savedFrame));
+
+    NSRect windowFrame = [webWindowController.window frame];
+    NSLog(@"windowFrame = %@", NSStringFromRect(windowFrame));
+    
+    XCTAssertTrue(NSRectEqualToRect(savedFrame, windowFrame), @"Rects should equal");
+    // If the window frame equals kTestWindowFrame, set the destination to kTestWindowFrameTwo
+    // Otherwise set the destination to kTestWindowFrame
+    // Test that the window from NSUserDefaults does not equal the destination frame
+    
+    // Test the window frame equals the frame from NSUserDefaults
+    
+    // Resize the window to the destination frame
+
+    // Test that the window frame frame from NSUserDefaults now equals the destination frame
+    
+    // Close the window
+
+    // Open a new window
+    
+    // Test that the window's frame now equals the destination frame
+    
+    // Open a second window
+    
+    // Test that the second window also has the same frame (I'll probably have to modify this to just test the size to account for cascading)
+    
+    
     // Clean up
     [WCLTaskTestsHelper blockUntilTasksAreRunning:webWindowController.tasks];
     [WCLTaskTestsHelper blockUntilTasksFinish:webWindowController.tasks];
+}
+
+#pragma mark - Helpers
+
++ (NSRect)savedFrameNamed:(NSString *)frameName
+{
+    NSString *key = [NSString stringWithFormat:@"NSWindow Frame %@", frameName];
+    NSString *frameString = [[NSUserDefaults standardUserDefaults] stringForKey:key];
+    return NSRectFromString(frameString);
 }
 
 @end
