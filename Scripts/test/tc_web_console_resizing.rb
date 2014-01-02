6e7a94da52597f1b0b93847ea4e2f77e321bc5df
Roben Kleene <robenk@gmail.com>
2014-01-01 22:50:13 -0500

    Size comparison helpers

--- Scripts/test/tc_web_console_resizing.rb
+++ Scripts/test/tc_web_console_resizing.rb
@@ -8,6 +8,7 @@ require TEST_CONSTANTS_FILE
 require TEST_HELPER_FILE
 
 
+
 class TestResizing < Test::Unit::TestCase
   HELLOWORLDPLUGIN_PATH = File.join(TEST_DATA_DIRECTORY, "HelloWorld.bundle")
   HELLOWORLDPLUGIN_NAME = "HelloWorld"
@@ -15,25 +16,62 @@ class TestResizing < Test::Unit::TestCase
     WebConsole::load_plugin(HELLOWORLDPLUGIN_PATH)
   end
 
+  WINDOWBOUNDS = "120, 230, 700, 500"
+  WINDOWBOUNDSTWO = "150, 200, 600, 600"
+
+  # Add methods to string in the context of this test class:
+  # Search for escape_javaScript for how to contextually add a method to a string
+  # WINDOWBOUNDS.height
+  # WINDOWBOUNDS.width
+  # WINDOWBOUNDS.size_matches(bounds)
   def test_quit_confirming_after_starting_short_second_task
-    # WebConsole::run_plugin(HELLOWORLDPLUGIN_NAME)
-    # 
-    # bounds = TestsHelper::window_bounds
+    WebConsole::run_plugin(HELLOWORLDPLUGIN_NAME)
+    
+    bounds = TestsHelper::window_bounds
+
+    puts bounds
+    puts WINDOWBOUNDS
+    puts WINDOWBOUNDS.height
+    puts WINDOWBOUNDS.width
+    puts WINDOWBOUNDS
+
+    puts bounds.size_matches(WINDOWBOUNDS)
+    puts bounds.size_matches(WINDOWBOUNDSTWO)
+
+    TestsHelper::set_window_bounds(WINDOWBOUNDS)
+
+    # Write a helper method to compare one bounds string to another, I just want to test that the sizes are accurate:
+    # width = xend - xstart
+    # height = yend - ystart
+    # width1 == width2
+    # height1 == height2
+    
+    # cascading offset equals 21 pixels horizontal and 23 pixels vertical
+
     # window_id = WebConsole::window_id_for_plugin(HELLOWORLDPLUGIN_NAME)
-    # 
-    # puts bounds
-    # puts window_id
+  end
 
-    TestsHelper::set_window_bounds("120, 230, 700, 500")
-   
-    # Move the window to a location
+  class ::String
+    def height
+      bounds = self.split(",")
+      return bounds[3].to_i - bounds[1].to_i
+    end
 
-    # Have AppleScript set the windows bounds
+    def width
+      bounds = self.split(",")
+      return bounds[2].to_i - bounds[0].to_i
+    end
 
-    # tell application "Web Console"
-    #   set the bounds of window 1 to {120, 230, 700, 500}
-    # end tell
+    def size_matches(bounds)
+      if self.width != bounds.width
+        return false
+      end
 
-  end  
+      if self.height != bounds.height
+        return false
+      end
 
+      return true
+    end
+  end
 end
