0b497bcb8c6aef14033e36537b2f04c44b468daa
Roben Kleene <robenk@gmail.com>
2014-01-04 17:12:58 -0500

    WINDOWID to WC_WINDOW_ID

--- Web Console/Web Console/WebConsoleConstants.h
+++ Web Console/Web Console/WebConsoleConstants.h
@@ -16,7 +16,7 @@
 
 #define kEnvironmentVariablePathKey @"PATH"
 #define kEnvironmentVariablePathValue @"/Users/robenkleene/.rbenv/shims:/Users/robenkleene/.rbenv/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";
-#define kEnvironmentVariableWindowIDKey @"WINDOWID"
+#define kEnvironmentVariableWindowIDKey @"WC_WINDOW_ID"
 #define kEnvironmentVariablePluginPathKey @"WC_PLUGIN_PATH"
 
 #define kTaskInterruptTimeout 1.0
