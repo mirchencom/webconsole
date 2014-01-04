869e579f1f52237be7b0283f8d1fd39b614c9976
Roben Kleene <robenk@gmail.com>
2014-01-04 17:10:08 -0500

    Replaced all instances of WINDOWID

--- Scripts/test/data/Print.bundle/Contents/Resources/wcprint/wcprint.rb
+++ Scripts/test/data/Print.bundle/Contents/Resources/wcprint/wcprint.rb
@@ -6,8 +6,12 @@ LIB_DIRECTORY = File.join(File.dirname(__FILE__), "lib")
 CONTROLLER_FILE = File.join(LIB_DIRECTORY, "controller")
 require CONTROLLER_FILE
 
+SHARED_RESOURCES_PATH = WebConsole::resource_path_for_plugin("Shared Resources")
+WEB_CONSOLE_CONSTANTS_FILE = File.join(SHARED_RESOURCES_PATH, "Shared/ruby/web_console_constants")
+require WEB_CONSOLE_CONSTANTS_FILE
+
 # Window Manager
-window_id = ENV['WINDOWID']
+window_id = ENV[WINDOW_ID_KEY]
 window_manager = WebConsole::WindowManager.new(window_id)
 BASE_PATH = File.expand_path(File.dirname(__FILE__))
 window_manager.base_url_path = BASE_PATH
--- Web Console/IRB/wcirb/lib/bridge.rb
+++ Web Console/IRB/wcirb/lib/bridge.rb
@@ -2,6 +2,10 @@
 
 require 'webconsole'
 
+SHARED_RESOURCES_PATH = WebConsole::resource_path_for_plugin("Shared Resources")
+WEB_CONSOLE_CONSTANTS_FILE = File.join(SHARED_RESOURCES_PATH, "Shared/ruby/web_console_constants")
+require WEB_CONSOLE_CONSTANTS_FILE
+
 CONTROLLER_FILE = File.join(File.dirname(__FILE__), "controller")
 require CONTROLLER_FILE
 
@@ -9,7 +13,7 @@ WINDOW_MANAGER_FILE = File.join(File.dirname(__FILE__), "window_manager")
 require WINDOW_MANAGER_FILE
 
 # Window Manager
-window_id = ENV['WINDOWID']
+window_id = ENV[WINDOW_ID_KEY]
 window_manager = WcIRB::WindowManager.new(window_id)
 
 # Controller
