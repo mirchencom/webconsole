0966ad8a867475b5df5e46e541a2357562489173
Roben Kleene <robenk@gmail.com>
2014-01-19 16:28:49 -0500

    Added filename to title

--- Web Console/Markdown/wcmarkdown/lib/controller.rb
+++ Web Console/Markdown/wcmarkdown/lib/controller.rb
@@ -7,9 +7,10 @@ module WcMarkdown
     VIEWS_DIRECTORY = File.join(BASE_DIRECTORY, "views")
     VIEW_TEMPLATE = File.join(VIEWS_DIRECTORY, 'view.html.erb')
 
-    def initialize(delegate = nil, markdown)
+    def initialize(delegate = nil, markdown = nil, filename = nil)
       @renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
       @html = @renderer.render(markdown)
+      @filename = filename
 
       super(delegate, VIEW_TEMPLATE)
     end
@@ -17,6 +18,13 @@ module WcMarkdown
     def body_content
       return @html
     end    
+
+    def title_content
+      if !@filename
+        return "Markdown Preview"
+      end
+      return @filename
+    end
 
     def markdown=(markdown)
       @html = @renderer.render(markdown)
--- Web Console/Markdown/wcmarkdown/wcmarkdown.rb
+++ Web Console/Markdown/wcmarkdown/wcmarkdown.rb
@@ -15,16 +15,19 @@ require WINDOW_MANAGER_FILE
 if !ARGV.empty?
   file = ARGF.file
 end
-
 markdown = ARGF.read
 
 window_manager = WcMarkdown::WindowManager.new
-controller = WcMarkdown::Controller.new(window_manager, markdown)
 
-exit if !file
+if !file
+  WcMarkdown::Controller.new(window_manager, markdown)
+  exit
+end
+
+filename = File.basename(file)
+controller = WcMarkdown::Controller.new(window_manager, markdown, filename)
 
 path = File.expand_path(File.dirname(file))
-filename = File.basename(file)
 
 listener = Listen.to(path, only: /^#{Regexp.quote(filename)}$/) { |modified, added, removed| 
   file = File.open(modified[0])
