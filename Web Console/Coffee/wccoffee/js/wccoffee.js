99887c8b24912eb7e2c031dbd6169f494e14c6bf
Roben Kleene <robenk@gmail.com>
2014-01-23 22:43:04 -0500

    Coffee syntax highlighting

--- Web Console/Coffee/wccoffee/js/wccoffee.js
+++ Web Console/Coffee/wccoffee/js/wccoffee.js
@@ -4,5 +4,13 @@ function addCode(code) {
 	var data = { 
 		code: code
 	};
-	$(template(data)).appendTo("body");
+
+	var $newcode = $(template(data)).appendTo("body");	
+	$(document).ready(function() {
+	  $newcode.each(function(i, e) {
+		  console.log("e = " + e);
+		  hljs.highlightBlock(e);
+	  });
+	});
 }
+
