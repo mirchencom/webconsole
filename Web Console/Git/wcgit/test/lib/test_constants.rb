0d25c85b7a27bb8537ba7baaa3435376e71b1d09
Roben Kleene <robenk@gmail.com>
2014-02-01 14:34:28 -0500

    Setup WcGit tests WindowManagerHelper

--- Web Console/Git/wcgit/test/lib/test_constants.rb
+++ Web Console/Git/wcgit/test/lib/test_constants.rb
@@ -1,7 +1,8 @@
 # Test
 TEST_LIB_DIRECTORY = File.dirname(__FILE__)
-GIT_TEST_HELPER_FILE = File.join(TEST_LIB_DIRECTORY, "git_test_helper")
+TEST_GIT_HELPER_FILE = File.join(TEST_LIB_DIRECTORY, "git_test_helper")
 TEST_DATA_FILE = File.join(TEST_LIB_DIRECTORY, "test_data")
+TEST_WINDOW_MANAGER_HELPER = File.join(TEST_LIB_DIRECTORY, "test_window_manager_helper")
 
 # Lib
 LIB_DIRECTORY = File.join(File.dirname(__FILE__), "..", "..", "lib")
--- Web Console/Git/wcgit/test/tc_controller.rb
+++ Web Console/Git/wcgit/test/tc_controller.rb
@@ -5,23 +5,24 @@ require "test/unit"
 TEST_CONSTANTS_FILE = File.join(File.dirname(__FILE__), "lib", "test_constants")
 require TEST_CONSTANTS_FILE
 
-require GIT_TEST_HELPER_FILE
-
+require TEST_GIT_HELPER_FILE
 require TEST_DATA_FILE
+
+require TEST_WINDOW_MANAGER_HELPER
 
 require CONTROLLER_FILE
 require WINDOW_MANAGER_FILE
 
 class TestController < Test::Unit::TestCase
 
-  # def test_class
-  #   git_test_helper = WcGit::GitTestHelper.new
-  # 
-  #   git_test_helper.add_file(TEST_FILE_ONE, TEST_FILE_ONE_CONTENT)
-  #   git_test_helper.add_file(TEST_FILE_TWO, TEST_FILE_TWO_CONTENT)
-  # 
-  #   git_test_helper.clean_up
-  # end
+  def test_class
+    git_helper = WcGit::Tests::GitHelper.new
+  
+    git_helper.add_file(TEST_FILE_ONE, TEST_FILE_ONE_CONTENT)
+    git_helper.add_file(TEST_FILE_TWO, TEST_FILE_TWO_CONTENT)
+  
+    git_helper.clean_up
+  end
 
   def test_set_branch
     window_manager = WcGit::WindowManager.new
@@ -29,7 +30,13 @@ class TestController < Test::Unit::TestCase
 
     branch_name = "master"
     controller.branch = branch_name
+    assert(branch_name = controller.branch, "The controller's branch should equal the branch name.")
 
+    branch_name = "development"
+    controller.branch = branch_name
+    window_manager_test_helper = WcGit::Tests::WindowManagerHelper.new(window_manager)
+    branch_element_count = window_manager_test_helper.element_count_for_selector("[id=branch]").to_i # Alternative ID selector syntax will select multiple elements with the same ID.
+    assert(branch_element_count == 1, "There should be one branch element.")
     assert(branch_name = controller.branch, "The controller's branch should equal the branch name.")
 
     window_manager.close
