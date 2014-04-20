496a579dd09f2459501b45687c684d2a39c74f05
Roben Kleene <robenk@gmail.com>
2014-04-20 14:48:55 -0400

    Removed seemingly superfluous set NODE_PATH

--- Web Console/PlugIns/Node.wcplugin/Contents/Resources/wcnode.rb
+++ Web Console/PlugIns/Node.wcplugin/Contents/Resources/wcnode.rb
@@ -2,7 +2,6 @@
 
 require File.join(File.dirname(__FILE__), "lib", "wrapper")
 
-ENV['NODE_PATH'] = "/usr/local/share/npm/lib/node_modules:/usr/local/lib/node_modules"
 wrapper = WcNode::Wrapper.new
 
 ARGF.each do |line|
