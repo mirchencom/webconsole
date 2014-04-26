f8a7df27cb84894544eae199db7e55f8adeebab7
Roben Kleene <robenk@gmail.com>
2014-04-26 15:24:42 -0400

    HTML webconsole update

--- Web Console/PlugIns/HTML.wcplugin/Contents/Resources/bundle/ruby/2.0.0/gems/webconsole-0.0.2/lib/webconsole.rb
+++
@@ -1,7 +0,0 @@
-module WebConsole
-  WEBCONSOLE_CONSTANTS = File.join(File.dirname(__FILE__), "webconsole", "constants")
-  require WEBCONSOLE_CONSTANTS
-  require WINDOW_MANAGER_FILE
-  require CONTROLLER_FILE
-  require MODULE_FILE
-end
--- Web Console/PlugIns/HTML.wcplugin/Contents/Resources/bundle/ruby/2.0.0/gems/webconsole-0.0.3/lib/webconsole/constants.rb
+++ Web Console/PlugIns/HTML.wcplugin/Contents/Resources/bundle/ruby/2.0.0/gems/webconsole-0.0.3/lib/webconsole/constants.rb
@@ -5,11 +5,6 @@ module WebConsole
   SHARED_RESOURCES_PATH_KEY = 'WC_SHARED_RESOURCES_PATH'
   SHARED_RESOURCES_URL_KEY = 'WC_SHARED_RESOURCES_URL'
 
-  # Files
-  WINDOW_MANAGER_FILE = File.join(File.dirname(__FILE__), "window_manager")
-  CONTROLLER_FILE = File.join(File.dirname(__FILE__), "controller")
-  MODULE_FILE = File.join(File.dirname(__FILE__), "module")
-  
   # Directories
   APPLESCRIPT_DIRECTORY = File.join(File.dirname(__FILE__), "..", "applescript")
 end
