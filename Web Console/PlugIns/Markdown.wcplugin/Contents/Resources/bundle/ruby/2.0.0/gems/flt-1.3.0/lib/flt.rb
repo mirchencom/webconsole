ab74d87e6ccb64543c5a7b6d27ba857ea3acba8b
Roben Kleene <robenkleene@Robens-Mac.local>
2014-05-04 16:14:28 -0700

    Fixes to get tests passing on virtual machine

---
+++ Web Console/PlugIns/Markdown.wcplugin/Contents/Resources/bundle/ruby/2.0.0/gems/flt-1.3.0/lib/flt.rb
@@ -0,0 +1,10 @@
+$:.unshift File.dirname(__FILE__)
+
+require 'flt/version'
+require 'flt/support'
+require 'flt/float'
+require 'flt/bigdecimal'
+require 'flt/num'
+require 'flt/dec_num'
+require 'flt/bin_num'
+require 'flt/tolerance'
