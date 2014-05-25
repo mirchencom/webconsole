c5f46723fbad5bde48c3c648c1ebb8f9f25d8ea4
Roben Kleene <robenk@gmail.com>
2014-05-24 20:28:15 -0400

    Add hack for handling doubled output in IRB

--- Web Console/PlugIns/IRB.wcplugin/Contents/Resources/lib/output_controller.rb
+++ Web Console/PlugIns/IRB.wcplugin/Contents/Resources/lib/output_controller.rb
@@ -3,7 +3,19 @@ require 'webconsole/repl'
 
 module WebConsole::REPL::IRB
   class OutputController < WebConsole::REPL::OutputController
+    def initialize
+      @SEEN_PROMPT = false
+    end
+
     def parse_output(output)
+
+      # Quick hack to fix a bug where on first run IRB is echoing the input twice
+      if !@SEEN_PROMPT && output =~ /^=>\s/
+        @SEEN_PROMPT = true
+      else
+        return
+      end
+
       if output =~ /^irb\([^)]*\):[^:]*:[^>]*(>|\*)/
         # Don't add echo of input
         return
