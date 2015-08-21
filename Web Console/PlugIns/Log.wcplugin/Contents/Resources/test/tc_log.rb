9ef934547a3bdaccc816dbfcd5b658cbc1492fc3
Roben Kleene <robenk@gmail.com>
2015-08-20 21:04:02 -0400

    Finish tests for log

--- Web Console/PlugIns/Log.wcplugin/Contents/Resources/test/tc_log.rb
+++ Web Console/PlugIns/Log.wcplugin/Contents/Resources/test/tc_log.rb
@@ -24,10 +24,10 @@ class TestPlugin < Test::Unit::TestCase
     @window = WebConsole::Window.new(window_id)
   end
 
-  # def teardown
-  #   WebConsole::Tests::Helper::quit
-  #   assert(!WebConsole::Tests::Helper::is_running, "The application should not be running.")
-  # end
+  def teardown
+    WebConsole::Tests::Helper::quit
+    assert(!WebConsole::Tests::Helper::is_running, "The application should not be running.")
+  end
 
   # Tests
 
@@ -37,21 +37,56 @@ class TestPlugin < Test::Unit::TestCase
     title.chomp!
     assert_equal(title, TEST_PLUGIN_NAME, "The title should equal the test html title.")
 
-    log_info("Testing log info")
+    # Test Info
+    message = "Testing log info"
+    log_info(message)
+    test_message = last_log_message()
+    assert_equal(message, test_message, "The messages should match")
+    test_class = last_log_class()
+    assert_equal("info", test_class, "The classes should match")
+
+    # Test Error
+    message = "Testing log error"
+    log_error(message)
+    test_message = last_log_message()
+    assert_equal(message, test_message, "The messages should match")
+    test_class = last_log_class()
+    assert_equal("error", test_class, "The classes should match")
+
+    # Test Warning
+    message = "Testing log warning"
+    log_warning(message)
+    test_message = last_log_message()
+    assert_equal(message, test_message, "The messages should match")
+    test_class = last_log_class()
+    assert_equal("warning", test_class, "The classes should match")
 
   end
 
   # Helper
+  def last_log_class()
+    result = @window.do_javascript(TEST_CLASS_JAVASCRIPT)
+    return result.chomp
+  end
+
+  def last_log_message()
+    result = @window.do_javascript(TEST_MESSAGE_JAVASCRIPT)
+    return result.chomp
+  end
 
   def log_info(message)
-    a_function = WebConsole::View::javascript_function("info", [message])
-    @window.do_javascript(a_function)
+    javascript = WebConsole::View::javascript_function("info", [message])
+    @window.do_javascript(javascript)
   end
 
-  def logError(message)
+  def log_error(message)
+    javascript = WebConsole::View::javascript_function("error", [message])
+    @window.do_javascript(javascript)
   end
 
-  def logWarning(message)
+  def log_warning(message)
+    javascript = WebConsole::View::javascript_function("warning", [message])
+    @window.do_javascript(javascript)
   end
 
 end
