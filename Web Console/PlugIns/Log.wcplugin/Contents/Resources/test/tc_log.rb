f0f3a88c719fc3562650ba9c8d622b5dc92432b3
Roben Kleene <robenk@gmail.com>
2015-08-20 20:00:52 -0400

    Setup JavaScript log calls

--- Web Console/PlugIns/Log.wcplugin/Contents/Resources/test/tc_log.rb
+++ Web Console/PlugIns/Log.wcplugin/Contents/Resources/test/tc_log.rb
@@ -11,28 +11,47 @@ require WebConsole::Tests::TEST_HELPER_FILE
 
 class TestPlugin < Test::Unit::TestCase
 
+  # Life Cycle
+  
   def setup
     WebConsole::load_plugin(TEST_PLUGIN_PATH)
+    # Run the plugin
+    WebConsole::run_plugin(TEST_PLUGIN_NAME)
+    
+    # Setup the window
+    sleep WebConsole::Tests::TEST_PAUSE_TIME # Give the plugin time to finish running
+    window_id = WebConsole::window_id_for_plugin(TEST_PLUGIN_NAME)
+    @window = WebConsole::Window.new(window_id)
   end
 
-  def teardown
-    # window.close
-    WebConsole::Tests::Helper::quit
-    assert(!WebConsole::Tests::Helper::is_running, "The application should not be running.")
-  end
+  # def teardown
+  #   WebConsole::Tests::Helper::quit
+  #   assert(!WebConsole::Tests::Helper::is_running, "The application should not be running.")
+  # end
+
+  # Tests
 
   def test_log
-    WebConsole::run_plugin(TEST_PLUGIN_NAME)
-
-    sleep WebConsole::Tests::TEST_PAUSE_TIME # Give the plugin time to finish running
-
-    window_id = WebConsole::window_id_for_plugin(TEST_PLUGIN_NAME)
-    window = WebConsole::Window.new(window_id)
-
-    title = window.do_javascript(TEST_TITLE_JAVASCRIPT)
+    # Confirm the title
+    title = @window.do_javascript(TEST_TITLE_JAVASCRIPT)
     title.chomp!
-
     assert_equal(title, TEST_PLUGIN_NAME, "The title should equal the test html title.")
+
+    log_info("Testing log info")
+
+  end
+
+  # Helper
+
+  def log_info(message)
+    a_function = WebConsole::View::javascript_function("info", [message])
+    @window.do_javascript(a_function)
+  end
+
+  def logError(message)
+  end
+
+  def logWarning(message)
   end
 
 end
