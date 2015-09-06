3b1705415788d50f0535886e95c9330766eb5588
Roben Kleene <robenk@gmail.com>
2015-09-05 22:02:28 -0400

    Make view a subclass of window, untested

--- webconsole/lib/webconsole/lib/view.rb
+++ webconsole/lib/webconsole/lib/view.rb
@@ -3,31 +3,48 @@ require_relative "view/erb"
 require_relative "view/resources"
 
 module WebConsole
-  class View
+  class View < Window
 
-    attr_reader :window
-    def initialize
-      @window = WebConsole::Window.new
+    # Properties
+
+    def view_id
+      if !@view_id
+        if ENV.has_key?(VIEW_ID_KEY)
+          @view_id = ENV[VIEW_ID_KEY]
+        else
+          @view_id = split_id
+        end
+      end
+      return @view_id
     end
 
-    def base_url=(value)
-      @window.base_url = value
-    end
+    # Web
 
-    def base_url_path=(value)
-      @window.base_url_path = value
-    end
-
+    LOAD_HTML_IN_SPLIT_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "load_html_in_split.scpt")
+    LOAD_HTML_WITH_BASE_URL_IN_SPLIT_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "load_html_with_base_url_in_split.scpt")
     def load_html(html)
-      @window.load_html(html)
+      arguments = [html]
+
+      script = LOAD_HTML_IN_SPLIT_SCRIPT
+
+      if @base_url
+        script = LOAD_HTML_WITH_BASE_URL_IN_SPLIT_SCRIPT
+        arguments.push(@base_url)
+      end
+
+      arguments.push(view_id)
+
+      WebConsole::run_applescript(script, arguments)
     end
 
+    DO_JAVASCRIPT_IN_SPLIT_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "do_javascript_in_split.scpt")
     def do_javascript(javascript)
-      return @window.do_javascript(javascript)
+      WebConsole::run_applescript(DO_JAVASCRIPT_IN_SPLIT_SCRIPT, [javascript, view_id])
     end
 
-    def close
-      @window.close
+    READ_FROM_STANDARD_INPUT_IN_SPLIT_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "read_from_standard_input_in_split.scpt")
+    def read_from_standard_input(text)
+      WebConsole::run_applescript(READ_FROM_STANDARD_INPUT_IN_SPLIT_SCRIPT, [text, view_id])
     end
 
   end
--- webconsole/lib/webconsole/lib/window.rb
+++ webconsole/lib/webconsole/lib/window.rb
@@ -8,9 +8,24 @@ module WebConsole
       @window_id = window_id
     end
 
+    # Properties
+
     def base_url_path=(value)
       @base_url = "file://" + value
     end
+    
+    def window_id
+      if !@window_id
+        if ENV.has_key?(WINDOW_ID_KEY)
+          @window_id = ENV[WINDOW_ID_KEY]
+        else
+          @window_id = WebConsole::create_window
+        end
+      end
+      return @window_id
+    end
+    
+    # Web
 
     LOADHTML_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "load_html.scpt")
     LOADHTMLWITHBASEURL_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "load_html_with_base_url.scpt")
@@ -24,15 +39,9 @@ module WebConsole
         arguments.push(@base_url)
       end
 
-      if window_id
-        arguments.push(window_id)
-      end
+      arguments.push(window_id)
 
-      result = WebConsole::run_applescript(script, arguments)
-      if !window_id
-        # This allows a window manager created without a window_id and then be assigned the window_id when load_html is called.
-        @window_id = self.class.window_id_from_result(result)        
-      end
+      WebConsole::run_applescript(script, arguments)
     end
 
     DOJAVASCRIPT_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "do_javascript.scpt")
@@ -45,22 +54,26 @@ module WebConsole
       WebConsole::run_applescript(READ_FROM_STANDARD_INPUT_SCRIPT, [text, window_id])
     end
 
+    # Window
+
     CLOSEWINDOW_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "close_window.scpt")
     def close
       WebConsole::run_applescript(CLOSEWINDOW_SCRIPT, [window_id])
     end
-    
-    def window_id
-      if !@window_id && ENV.has_key?(WINDOW_ID_KEY)
-        @window_id = ENV[WINDOW_ID_KEY]
-      end
-      return @window_id
-    end
 
-    private
-    
-    def self.window_id_from_result(result)
-      return result.split.last.to_i
+    # Splits
+
+    SPLIT_ID_IN_WINDOW_SCRIPT = File.join(APPLESCRIPT_DIRECTORY, "split_id_in_window.scpt")
+    def split_id(pluginName = nil)
+      arguments = [window_id]
+
+      if pluginName
+        arguments.push(pluginName)
+      end
+
+      result = self.run_applescript(SPLIT_ID_IN_WINDOW_SCRIPT, [arguments])
+      result.chomp!
+      return result
     end
 
   end
