5e0b29c4f8d9ccf37262bb4c855dcdcff0cddd58
Roben Kleene <robenk@gmail.com>
2015-11-10 22:02:05 -0500

    Flesh out test_log

--- Web Console/Plugins/Shared Test Resources.wcplugin/Contents/Resources/plugin/TestLog.wcplugin/Contents/Resources/test_log.rb
+++ Web Console/Plugins/Shared Test Resources.wcplugin/Contents/Resources/plugin/TestLog.wcplugin/Contents/Resources/test_log.rb
@@ -6,5 +6,9 @@ require 'webconsole/logger'
 
 logger = WebConsole::Logger.new
 logger.info("Testing log message")
+puts "Testing print to standard input"
+STDERR.puts "Testing print to standard error"
+window = WebConsole::Window.new
+window.do_javascript("document.body.innerHTML = \"test\";")
 logger.error("Testing log error")
 logger.show
