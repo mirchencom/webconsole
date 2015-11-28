95566edd92e717010c5e8825a9750087b21c0d79
Roben Kleene <robenk@gmail.com>
2015-11-28 17:39:46 -0500

    Add WCLPluginTaskTests

---
+++ Web Console/Web ConsoleTests/WCLPluginTaskTests.swift
@@ -0,0 +1,22 @@
+//
+//  WCLPluginTaskTests.swift
+//  Web Console
+//
+//  Created by Roben Kleene on 11/28/15.
+//  Copyright © 2015 Roben Kleene. All rights reserved.
+//
+
+import XCTest
+
+class WCLPluginTaskTests: XCTestCase, WCLPluginTaskDelegate {
+
+//    func testExample() {
+//        let expection = expectationWithDescription("Run task")
+//        WCLPluginTask.runTaskWithCommandPath("invalid path", withArguments: nil, inDirectoryPath: nil, delegate: self) { (success) -> Void in
+//            XCTAssertFalse(success)
+//            expection.fulfill()
+//        }
+//        waitForExpectationsWithTimeout(testTimeout, handler: nil)
+//    }
+
+}
