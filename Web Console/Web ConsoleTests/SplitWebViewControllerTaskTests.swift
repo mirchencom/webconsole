2d9bc5625f8ec874995204a714111f137bea76e7
Roben Kleene <robenk@gmail.com>
2015-11-28 17:14:14 -0500

    Add SplitWebViewControllerTaskTests

---
+++ Web Console/Web ConsoleTests/SplitWebViewControllerTaskTests.swift
@@ -0,0 +1,9 @@
+//
+//  SplitWebViewControllerTaskTests.swift
+//  Web Console
+//
+//  Created by Roben Kleene on 11/28/15.
+//  Copyright © 2015 Roben Kleene. All rights reserved.
+//
+
+import Foundation
