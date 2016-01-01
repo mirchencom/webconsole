2c0d738c17e0de59d4168c6303363021f37279f1
Roben Kleene <robenk@gmail.com>
2016-01-01 13:10:46 -0500

    Add ProcessIntegrationTests

---
+++ Web Console/Web ConsoleTests/ProcessIntegrationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ProcessIntegrationTest.swift
+//  Web Console
+//
+//  Created by Roben Kleene on 1/1/16.
+//  Copyright © 2016 Roben Kleene. All rights reserved.
+//
+
+import XCTest
+
+class ProcessIntegrationTests: XCTestCase {
+
+    func testProcess() {
+        // Run a process with something as a delegate that stores that process in the process manager
+        // Call a function, running process info matching process info
+    
+    
+    }
+
+}
