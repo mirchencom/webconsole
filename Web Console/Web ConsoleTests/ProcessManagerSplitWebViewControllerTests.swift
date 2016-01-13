83a71e951e6a4a4c3323b1a61450aa39d3339549
Roben Kleene <robenk@gmail.com>
2016-01-12 20:09:06 -0500

    Add ProcessManagerSplitWebViewControllerTests

---
+++ Web Console/Web ConsoleTests/ProcessManagerSplitWebViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProcessManagerSplitWebViewControllerTests.swift
+//  Web Console
+//
+//  Created by Roben Kleene on 1/12/16.
+//  Copyright © 2016 Roben Kleene. All rights reserved.
+//
+
+import Foundation
+
+//class ProcessManagerSplitWebViewControllerTests: WebViewControllerEventRouterTestCase {
+//    
+//    
+//
+//
+//}
