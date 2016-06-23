3c44b74ded47ab600f04895bcbbc38d692a44aa2
Roben Kleene <robenk@gmail.com>
2016-06-22 21:36:06 -0400

    Fortify test task finishing code

--- Web Console/Web ConsoleTests/XCTestCase+Tasks.swift
+++ Web Console/Web ConsoleTests/XCTestCase+Tasks.swift
@@ -22,23 +22,29 @@ extension XCTestCase {
                 expectation = expectationWithDescription("Tasks terminated")
             }
 
+            let clearObserver: (NSObjectProtocol) -> () = { observer in
+                NSNotificationCenter.defaultCenter().removeObserver(observer)
+                observers.removeObject(observer)
+                if let expectation = expectation where observers.count == 0 {
+                    expectation.fulfill()
+                }
+            }
+        
             var observer: NSObjectProtocol?
             observer = NSNotificationCenter.defaultCenter().addObserverForName(NSTaskDidTerminateNotification,
                 object: task,
                 queue: nil)
                 { notification in
-
                     if let observer = observer {
-                        NSNotificationCenter.defaultCenter().removeObserver(observer)
-                        observers.removeObject(observer)
-                    }
-                    if let expectation = expectation where observers.count == 0 {
-                        expectation.fulfill()
+                        clearObserver(observer)
                     }
             }
-
+            
             if let observer = observer {
                 observers.addObject(observer)
+                if !task.running {
+                    clearObserver(observer)
+                }
             }
 
         }
