807631778dfbd0d54b9eb8b5e3e64c03f9b9e53a
Roben Kleene <robenk@gmail.com>
2017-05-25 22:48:57 -0400

    Add file extension test constants

---
+++ Web Console/Web ConsoleTests/FileExtensionsTestConstants.swift
@@ -0,0 +1,12 @@
+//
+//  FileExtensionsTestConstants.swift
+//  .
+//
+//  Created by Roben Kleene on 5/25/17.
+//  Copyright (c) 2017 Roben Kleene. All rights reserved.
+//
+
+let testFileExtensionEnabledKeyPath = "enabled"
+let testFileExtensionPluginIdentifierKey = "pluginIdentifier"
+let testFileExtensionSelectedPluginKeyPath = "selectedPlugin"
+let testFileExtensionPluginsKey = "plugins"
